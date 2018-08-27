f9654174f07fa52f6ca48ab31a61198dc971ec91
ronen801 <42750766+ronen801@users.noreply.github.com>
2018-08-27 22:23:08 +0300

    hi

---
+++ vrchat/Assets/Gman Asset files/snark/Flashlight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.F))
+			this.GetComponent<Light> ().enabled = true;
+		if (Input.GetKey (KeyCode.G))
+			this.GetComponent<Light> ().enabled = false;
+	}
+}
